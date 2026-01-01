63a9e6624cb4eb4676a9c25cacb9a1d14eda77e7
jimmy <jhattenhauer@outlook.com>
2026-01-01 13:10:35 -0330

    added solution

---
+++ 66_PlusOne/PlusOne.cpp
@@ -0,0 +1,11 @@
+#include <cmath>
+
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        int digit_count = digits.size();
+        for (int i = digit_count; i > 0; i--){
+
+        }
+    }
+};
