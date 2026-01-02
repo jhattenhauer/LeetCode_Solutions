179e5a85d913802c886ec0912c0d8614d3caff83
jimmy <jhattenhauer@outlook.com>
2026-01-02 16:37:52 -0330

    finished p66

--- 66_PlusOne/PlusOne.cpp
+++ 66_PlusOne/PlusOne.cpp
@@ -3,14 +3,15 @@ public:
     vector<int> plusOne(vector<int>& digits) {
         int digit_count = digits.size();
         int indecie = digit_count - 1;
-        for (int i = indecie; i > 0; i--){
-            if (digits[indecie] == 9){
-                ;
+        for (int i = indecie; i >= 0; i--){
+            if (digits[i] == 9){
+                digits[i] = 0;
             } else {
                 digits[i] += 1;
                 return digits;
             }
-
         }
+        digits.insert(digits.begin(), 1);
+        return digits;
     }
 };
