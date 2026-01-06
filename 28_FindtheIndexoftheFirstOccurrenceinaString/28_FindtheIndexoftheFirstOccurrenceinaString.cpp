e2698193ee8463a6e37604c6623efa6f528fdcfb
jimmy <jhattenhauer@outlook.com>
2026-01-06 15:46:49 -0330

    working on 28

---
+++ 28_FindtheIndexoftheFirstOccurrenceinaString/28_FindtheIndexoftheFirstOccurrenceinaString.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        bool found = false;
+        int len_word = needle.size();
+        
+        for (int i = 0; )
+
+    }
+};
+
+'''
+sliding window of needle over haystack finding keyword
+using position + end of word to not go over
+'''
