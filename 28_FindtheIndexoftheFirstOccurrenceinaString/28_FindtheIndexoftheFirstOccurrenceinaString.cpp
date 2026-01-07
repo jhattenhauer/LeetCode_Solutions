780a3237c1f201c92f00394d3f256c9adcf8d735
jimmy <jhattenhauer@outlook.com>
2026-01-07 10:16:20 -0330

    solved 28

--- 28_FindtheIndexoftheFirstOccurrenceinaString/28_FindtheIndexoftheFirstOccurrenceinaString.cpp
+++ 28_FindtheIndexoftheFirstOccurrenceinaString/28_FindtheIndexoftheFirstOccurrenceinaString.cpp
@@ -1,15 +1,24 @@
+#include <string>
+#include <iostream>
+
 class Solution {
 public:
-    int strStr(string haystack, string needle) {
-        bool found = false;
+    int strStr(std::string haystack, std::string needle) {
         int len_word = needle.size();
-        
-        for (int i = 0; )
+        int len_stack = haystack.size();
 
+        if (needle == haystack){return 0;}
+        
+        for (int i = 0; i + len_word < len_stack + 1; i++){
+            int to = i + len_word;
+            int from = i;
+            std::string window = haystack.substr(from, to - from);
+            std::cout << window << std::endl;
+            if (window == needle){
+                return i;
+            }
+        }
+
+        return -1;
     }
 };
-
-'''
-sliding window of needle over haystack finding keyword
-using position + end of word to not go over
-'''
